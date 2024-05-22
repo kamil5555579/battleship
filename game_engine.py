4f6b7d90997f91ba1564b4337268623f05720086
TymonOrl <01169766@pw.edu.pl>
2024-05-22 21:19:24 +0200

    Updates to game_engine.py
    fixed Ui, Bugs

--- game_engine.py
+++ game_engine.py
@@ -1,6 +1,9 @@
 import numpy as np
 import time
 
+'''
+Types of boats that will be used in game
+'''
 boat_types = {
     # "carrier" : 5,
     # "battleship": 4,
@@ -9,7 +12,11 @@ boat_types = {
     "destroyer": 2
 }
 
+
 class ConnectionErrorWithConn(Exception):
+    '''
+    Class to handle errors
+    '''
     def __init__(self, conn, message="Connection error occurred"):
         super().__init__(message)
         self.conn = conn
@@ -25,7 +32,7 @@ def get_indexes_from_player(conn, prompt: str):
         if indexes != -1:
             break
         send_message(conn, 'error')
-        time.sleep(0.1) # 100ms
+        time.sleep(0.1) # 100ms Gives time for response
 
     return indexes
 
@@ -38,9 +45,16 @@ def send_prompt_and_get_response(conn, prompt: str):
     return data.decode()
 
 def send_message(conn, message: str):
+    '''
+    Function that sends message to chosen client
+    '''
     conn.sendall(message.encode())
 
 def convert_to_index(in_str: str):
+        '''
+        Function that converts string like "A1" to coordinats x,y used in Matrixes.
+        It returns -1 if string was wrongly formated.
+        '''
         # Number convertion
         # Safety for wrong input
         if len(in_str) > 3:
@@ -74,7 +88,14 @@ def convert_to_index(in_str: str):
 
 
 class Game:
+    '''
+    Class holding core game loop. It checks if any player achieved victory by 
+    comparing players hit to amount of hits required for victory.
+    '''
     def __init__(self, player1, player2):
+        '''
+        Initialisation takes 2 Player classes that, have their boards set.
+        '''
         self.amount_of_winning_hits = np.array(list(boat_types.values())).sum()
         self.active_player = False
         self.players = (player1, player2)
@@ -89,16 +110,24 @@ class Game:
         send_message(self.players[self.active_player].conn , self.players[int(self.active_player)].show_board())
 
     def shoot(self):
+        '''
+        Takes shot from active player and makes sure it's possible by comparing 
+        it first to shots that have been already made, then calls check_hit method
+        on other player to see if it has succeded.
+        '''
         while(True):
             try:
                 indexes = get_indexes_from_player(self.players[self.active_player].conn, "shoot")
+                # To delete
+                print(not self.players[self.active_player].is_already_shot(indexes))
             except:
                 print("Error while getting response")
                 raise ConnectionError
             if not self.players[self.active_player].is_already_shot(indexes):
                 break
             else:
-                send_message(self.conn, 'You did that shot already!\n')
+                send_message(self.players[self.active_player].conn, 'You did that shot already!')
+            time.sleep(0.1) # 100ms Gives time for response
 
 
         if self.players[int(not self.active_player)].check_hit(indexes) == 1:
@@ -107,6 +136,9 @@ class Game:
             self.players[self.active_player].miss(indexes)
 
     def did_player_won(self):
+        '''
+        Function compares amount of hits needed with amount of hits player has.
+        '''
         print(self.players[self.active_player].get_amount_of_hits())
         print(self.amount_of_winning_hits)
         if self.players[self.active_player].get_amount_of_hits() \
@@ -116,11 +148,20 @@ class Game:
             return False
 
     def start(self):
+        '''
+        Main game loop: 
+            1. Show boards
+            2. Taking shot
+            3. Checking if any player won
+            4. if not change player
+        '''
         while(True):
             print(f"Player {int(self.active_player) + 1} shoots")
+            send_message(self.players[self.active_player].conn, 'Twoje rozstawianie')
             self.show_board()
+            send_message(self.players[self.active_player].conn, 'Twoje strzaly')
             self.show_shots()
-            time.sleep(0.1) # 100ms
+            time.sleep(0.1) # 100ms Gives time for response
             try:
                 self.shoot()
             except:
@@ -131,11 +172,14 @@ class Game:
                 send_message(self.players[self.active_player].conn,win_msg)
                 send_message(self.players[not self.active_player].conn,win_msg)
                 print(win_msg)
-                time.sleep(0.1) # 100ms
+                time.sleep(0.1) # 100ms Gives time for response
                 break
             self.change_player()
 
 class Player:
+    '''
+    Player class keeps track of hits, client socket and 2 boards; Shots and ships
+    '''
     def __init__(self, conn):
         self.players_shots = Shots()
         self.hit_counter = 0
@@ -171,15 +215,19 @@ class Player:
         return self.players_shots.is_already_shot(indexes)
 
 class Ship_placement:
+    '''
+    Class that takes care of initialisation of board for a Player.
+    '''
     def __init__(self, conn, board=None):
         self.conn = conn
 
         if board == None:
             # first index number(row) second letter column
-            self.ship_placements = np.zeros([10,10])
+            self.ship_placements = np.ones([10,10])*-1
 
             lst_of_boat_types = list(boat_types.keys())
             i = 0
+            # Loop through dictionary of Boats
             while(i<len(lst_of_boat_types)):
                 # print(f"Please place your: {lst_of_boat_types[i]} (size: {boat_types[lst_of_boat_types[i]]})")
                 # position = get_indexes_from_player('Staring index (A-Z and 1-10): ')
@@ -204,56 +252,65 @@ class Ship_placement:
         return board_to_str(self.ship_placements)
 
     def place_boat(self, boat_type: str, starting_indexes, rotation: str): # H - horizontal V - vertical
+        '''
+        Method which makes sure boats are not placed on each other and gives
+        possibility of rotating them. 
+        '''
         length = boat_types[boat_type]
         x = starting_indexes[1]
         y = starting_indexes[0]
 
-        #if (y+length)>9 or (x+length)>9:
-         #   send_message(self.conn, 'error too long')
-         #   time.sleep(0.1) # 100ms
-         #   return -1 
-
-
         if rotation == "v" or rotation == "V":
             if (x+length-1)>9:
                 send_message(self.conn, 'error too long')
-                time.sleep(0.1) # 100ms
+                time.sleep(0.1) # 100ms Gives time for response
                 return -1 
             elif np.any(self.ship_placements[ \
                 x:x+length, \
-                y] == 1):
+                y] == 0):
                     send_message(self.conn, 'error')
-                    time.sleep(0.1) # 100ms
+                    time.sleep(0.1) # 100ms Gives time for response
                     return -1
             else:
                 self.ship_placements[ \
                     x:x+length, \
-                    y] = 1
+                    y] = 0
         else:
             if(y+length-1)>9:
                 send_message(self.conn, 'error too long')
-                time.sleep(0.1) # 100ms
+                time.sleep(0.1) # 100ms Gives time for response
                 return -1 
             if np.any(self.ship_placements[ \
                 x, \
-                y:y+length] == 1):
+                y:y+length] == 0):
                     send_message(self.conn, 'error')
-                    time.sleep(0.1) # 100ms
+                    time.sleep(0.1) # 100ms Gives time for response
                     return -1
             else:
                 self.ship_placements[ \
                     x, \
-                    y:y+length] = 1
+                    y:y+length] = 0
 
     def check_hit(self, attacked_place):
+        '''
+        Method that returns 1 on hit and 0 on miss and updates board to mark a
+        hit.
+        '''
         x = attacked_place[1]
         y = attacked_place[0]
-        if (self.ship_placements[x,y]==1):
+        if (self.ship_placements[x,y]==0):
+            self.ship_placements[x,y] = 1
             return 1
         else:
             return 0
 
 class Shots:
+    '''
+    Board of shots that player made. 
+    -1 - shot has not been fired there
+    0  - it was a miss
+    1  - it was a hit
+    '''
     def __init__(self):
         self.board = np.ones([10,10])*-1
 
@@ -279,6 +336,12 @@ class Shots:
         self.board[x,y] = 0
 
 def board_to_str(array):
+    '''
+    Function that converts numpy array to string with boarders top 1-10 left A-J
+    -1 - '.'
+    0  - 'O'
+    1  - 'X'
+    '''
     N = array.shape[0]
     output_str = ""
 
@@ -294,27 +357,16 @@ def board_to_str(array):
                 match array[i-1,j-1]:
 
                     case -1:
-                        output_str += chr(35) # #
+                        output_str += '.' # #
 
                     case 0:
-                        output_str += chr(79) # O
+                        output_str += 'O' # O
 
                     case 1:
-                        output_str += chr(88) # X
+                        output_str += 'X' # X
 
             output_str += ' '
 
         output_str += '\n'
  
     return output_str
-
-
-# Main
-'''
-test_array = np.array([[0,0,0],
-                        [0,1,0],
-                        [0,0,-1]])
-print(board_to_str(test_array))
-'''
-# game1 = Game()
-# game1.start()
--- server.py
+++ server.py
@@ -11,6 +11,8 @@ def start_server() -> socket.socket:
     Creates and starts a server socket.
     """
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
     server_address = ('localhost', 12345)
     server_socket.bind(server_address)
     server_socket.listen()
